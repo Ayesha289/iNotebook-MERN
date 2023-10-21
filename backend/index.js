2934635f82dade9c2b8ec27b46b0038f0264dad3
Ayesha <ayeshamulani928@gmail.com>
2023-10-21 21:35:04 +0530

    MongoDB cloud and express server setup

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const run = require('./db');
+const express = require('express')
+const app = express()
+const port = 3000
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
