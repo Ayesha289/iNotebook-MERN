10dcb4bff71d1a28acf58549428cb38381520466
Ayesha <ayeshamulani928@gmail.com>
2023-10-22 10:59:54 +0530

    Added data validation

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 const connectToMongo = require('./db');
 const express = require('express');
-
 const app = express()
 const port = 3000
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-router.post('/', (req, res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
+
+router.post('/', [
+    body('name', 'Enter valid name').isLength({min: 3}),
+    body('email', 'Enter valid email').isEmail(),
+    body('password', 'Password must be atleast 5 characters').isLength({min: 5})
+], (req, res)=>{
+    // console.log(req.body);
+    // const user = User(req.body);
+    // user.save();
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    }).then(user => res.json(user))
+    .catch(err=> console.log(err),
+    res.json({error: "Please enter a unique value for email"}));
 });
 
 module.exports = router
