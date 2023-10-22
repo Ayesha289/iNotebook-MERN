ec1e3f6923c589ad102236cbb6413638465ac4cf
Ayesha <ayeshamulani928@gmail.com>
2023-10-22 12:42:03 +0530

    Created a login endpoint

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,14 +7,13 @@ const { body, validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
 
 
+//creating a user
+
 router.post('/createUser', [
     body('name', 'Enter valid name').isLength({min: 3}),
     body('email', 'Enter valid email').isEmail(),
     body('password', 'Password must be atleast 5 characters').isLength({min: 5})
 ], async (req, res)=>{
-    // console.log(req.body);
-    // const user = User(req.body);
-    // user.save();
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()});
@@ -42,12 +41,41 @@ router.post('/createUser', [
         res.json(authtoken);
     }catch{
         console.error(error.message);
-        res.status(500).send("Some error occured");
+        res.status(500).send("Internal Server Error");
     }
-    
-    // .then(user => res.json(user))
-    // .catch(err=> console.log(err),
-    // res.json({error: "Please enter a unique value for email"}));
 });
+
+//authenticate a user
+router.post('/login', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password cannot be blank').exists(),
+], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    const {email, password} = req.body;
+    try{
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error: "Please try to login with correct credentials!"});
+        }
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error: "Please try to login with correct credentials!"});
+        }
+        const data = {
+            user:{
+                id: user.id
+            }
+        }
+
+        const authtoken = jwt.sign(data, process.env.JWT_SECRET);
+        res.json(authtoken);
+    }catch{
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+})
 
 module.exports = router
